6261cb07311eabe5d4c28376a91a651cedea1eb2
Subham Bhattacharya <subham.bh@icloud.com>
2025-12-08 19:45:39 +0530

    Add max_in_list.py to find largest number in a list
    This program takes a list of integers and prints the maximum value.

---
+++ easy/max_in_list.py
@@ -0,0 +1,7 @@
+# Find the largest number in a list
+
+numbers = [12, 45, 7, 23, 89, 34]  # You can modify this list
+
+maximum = max(numbers)
+
+print(f"The largest number in the list is: {maximum}")
